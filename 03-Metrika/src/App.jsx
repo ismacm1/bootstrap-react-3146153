7857007eefa1f647c1345141129f6d4cf6d50265
CLAV890 <kamiloavendano@gmail.com>
2025-11-18 13:24:04 -0500

    Metrika Avance

---
+++ 03-Metrika/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { SectionCards } from './components/SectionCards.jsx'
+
+
+export const App = () => {
+  return (
+    <>
+    <div></div>
+    <SectionCards/>
+    </>
+  )
+}
+
+
+
---
+++ 03-Metrika/src/components/SectionCards.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SectionCards = () => {
+  return (
+    <div>
+      <h1>HOLA MUNDO</h1>
+    </div>
+  )
+}
+
+export default SectionCards
+
+
+
